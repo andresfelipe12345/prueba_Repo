f45b29110152eb301ca37cd358399d2d9eaf6933
andresrodriguez <andresfelipe12345@hotmail.com>
2020-12-26 09:30:29 -0300

    Updates

---
+++ precache-manifest.b39cb3a5dc8d62b6f999c2e4513e7974.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d451f3f8fed0ff917fac40ed4f2d343",
+    "url": "/prueba_Repo/index.html"
+  },
+  {
+    "revision": "37457d9355777e928b71",
+    "url": "/prueba_Repo/static/css/main.79cd1c7d.chunk.css"
+  },
+  {
+    "revision": "0183b4dd1d3f6c4f7d9d",
+    "url": "/prueba_Repo/static/js/2.f8905dc2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/prueba_Repo/static/js/2.f8905dc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37457d9355777e928b71",
+    "url": "/prueba_Repo/static/js/main.98f40882.chunk.js"
+  },
+  {
+    "revision": "a8eb6042003a67b2b573",
+    "url": "/prueba_Repo/static/js/runtime-main.a573d4f4.js"
+  }
+]);
